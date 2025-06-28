98f1a9ace4fe33d3ebf77eeb3387d5e4d4837f46
Ian Ramirez <ianramirez@Ians-MacBook-Pro.local>
2025-06-28 18:25:32 -0400

    deploy build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project3/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './', // 👈 add this line here
 });
